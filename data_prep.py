b4d180b79ea7c7157cba314a48ed305ffb957b23
Ayush Shirsat <ayush34@bu.edu>
2019-04-04 02:09:39 -0400

    added labels for multiclass

--- data_prep.py
+++ data_prep.py
@@ -7,17 +7,16 @@ length = len(data)
 path = './train/'
 
 X = data[1:length,0]
-Y = data[1:length,1]
+Y = data[1:length,2:12]
 
 with open("X.csv", 'w', newline = '') as f:
     writer = csv.writer(f, delimiter =',')
     for i in range(length-1):
         filename = X[i]
-        img = cv2.imread(path+filename)
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        gray = cv2.resize(gray,(96,96))
-        flat_gray = gray.flatten()
-        writer.writerow(flat_gray)
+        img = cv2.imread(path+filename,1)
+        img_re = cv2.resize(img,(224,224))
+        flat_img = img_re.flatten()
+        writer.writerow(flat_img)
         print(i)
         
 with open("Y.csv", 'w', newline = '') as f:
